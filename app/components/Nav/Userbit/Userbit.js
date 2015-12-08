7da2310c82143e3bf5f330e86a09a082a190e288
GryphonEDM <griffinvmichael@gmail.com>
2015-12-07 16:25:03 -0800

    added space between email + pass fields

--- app/components/Nav/Userbit/Userbit.js
+++ app/components/Nav/Userbit/Userbit.js
@@ -85,7 +85,7 @@ var Userbit = React.createClass({
             return (
                 <div className="form-inline">
                   <div className="form-group" id="emailpassfields">
-                    <div className="input-group margin-bottom-sm">
+                    <div className="input-group margin-bottom-sm" id="emailbox">
                       <span className="input-group-addon"><i className="fa fa-envelope-o fa-fw"></i></span>
                       <input className="form-control" type="email" id="emailInput" ref="email" />
                     </div>
