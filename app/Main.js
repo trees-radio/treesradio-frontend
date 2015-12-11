694fcfd0453e3f01b6ed98091fbb5a1834fe7984
GryphonEDM <griffinvmichael@gmail.com>
2015-12-11 11:41:09 -0800

    Revert "added waitlists component"
    This reverts commit b75534f991479c52d0664fda88b38529003ed1be.

--- app/Main.js
+++ app/Main.js
@@ -16,7 +16,6 @@ import Nav from './components/Nav/Nav.js';
 import Chat from './components/Chat/Chat.js';
 import Video from './components/Video/Video.js';
 import Playlists from './components/Playlists/Playlists.js';
-import Waitlist from './components/Waitlist/Waitlist.js';
 
 // (S)CSS
 import './Main.scss';
@@ -194,9 +193,6 @@ var Main = React.createClass({
                           </div>
                           <div id="playlists-container">
                             <Playlists />
-                          </div>
-                          <div id="joinwaitlist-container">
-                            <Waitlist />
                           </div>
                       </div>
             {/* Chat Component */}
