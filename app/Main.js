8e8460e6be2d869fb44c17886797c5b3a0d7b574
Zach Buttram <zach.buttram@gmail.com>
2015-12-21 22:25:46 -0800

    Playlist truncation to 23 characters

--- app/Main.js
+++ app/Main.js
@@ -271,7 +271,7 @@ var Main = React.createClass({
       }
       sweetAlert({
         title: "New Playlist",
-        text: "Choose a name for your playlist!",
+        text: "Choose a name for your playlist!\n Note: Playlists will be limited to 23 characters.",
         type: "input",
         showCancelButton: true,
         closeOnConfirm: false,
@@ -338,6 +338,10 @@ var Main = React.createClass({
       let currentAuth = base.getAuth();
       // console.log(this.state.playlists[index].name);
       let nameToSelect = this.state.playlists[index].name;
+      if (nameToSelect.length > 23) {
+        let maxLength = 23;
+        nameToSelect = nameToSelect.substring(0,maxLength) + "...";
+      }
       let indexToSelect = index;
       this.setState({ currentPlaylist: {
         name: nameToSelect,
