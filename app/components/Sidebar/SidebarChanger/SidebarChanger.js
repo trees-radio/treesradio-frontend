90b7143c0458f48d6744575cb0b6ee41779d8be8
GryphonEDM <griffinvmichael@gmail.com>
2015-12-22 21:36:53 -0800

    css'd sidebar switcher begun

--- app/components/Sidebar/SidebarChanger/SidebarChanger.js
+++ app/components/Sidebar/SidebarChanger/SidebarChanger.js
@@ -6,8 +6,8 @@ var SidebarChanger = React.createClass({
   render: function() {
     return (
       <div className="row sidebar-changer">
-        <div className="show-chat-btn col-lg-3">Chat</div>
-        <div className="show-ousers-btn col-lg-3">Online Users</div>
+        <div className="show-chat-btn col-lg-3 sidebar-selected">Chat</div>
+        <div className="show-ousers-btn col-lg-3">Online Ents</div>
         <div className="show-waitlist-btn col-lg-3">Waitlist</div>
         <div className="show-about-btn col-lg-3">About</div>
       </div>
