ce765d8a63eb153a9349a026464f6005e81e56ac
Zach Buttram <zach.buttram@gmail.com>
2015-12-29 22:06:48 -0800

    Some work on dynamic classes

--- app/components/Sidebar/SidebarChanger/SidebarChanger.js
+++ app/components/Sidebar/SidebarChanger/SidebarChanger.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import classNames from 'classnames';
 
 var SidebarChanger = React.createClass({
   switchChat: function() {
@@ -15,12 +16,37 @@ var SidebarChanger = React.createClass({
     this.props.changeSidebar(3);
   },
   render: function() {
+    let chatSelected;
+    let onlineSelected;
+    let waitlistSelected;
+    let aboutSelected;
+
+    let chatBtnClass = classNames(
+      'show-chat-btn',
+      'col-lg-3',
+      { 'sidebar-selected': chatSelected }
+    );
+    let onlineBtnClass = classNames(
+      'show-ousers-btn',
+      'col-lg-3',
+      { 'sidebar-selected': onlineSelected }
+    );
+    let waitlistBtnClass = classNames(
+      'show-waitlist-btn',
+      'col-lg-3',
+      { 'sidebar-selected': waitlistSelected }
+    );
+    let aboutBtnClass = classNames(
+      'show-about-btn',
+      'col-lg-3',
+      { 'sidebar-selected': aboutSelected }
+    );
     return (
       <div className="row sidebar-changer">
-        <div className="show-chat-btn col-lg-3 sidebar-selected" ref="sel-chat" onClick={this.switchChat}>Chat</div>
-        <div className="show-ousers-btn col-lg-3" ref="sel-online" onClick={this.switchOnline}>Online Ents</div>
-        <div className="show-waitlist-btn col-lg-3" ref="sel-waitlist" onClick={this.switchWaitlist}>Waitlist</div>
-        <div className="show-about-btn col-lg-3" ref="sel-about" onClick={this.switchAbout}>About</div>
+        <div className={chatBtnClass} ref="sel-chat" onClick={this.switchChat}>Chat</div>
+        <div className={onlineBtnClass} ref="sel-online" onClick={this.switchOnline}>Online Ents</div>
+        <div className={waitlistBtnClass} ref="sel-waitlist" onClick={this.switchWaitlist}>Waitlist</div>
+        <div className={aboutBtnClass} ref="sel-about" onClick={this.switchAbout}>About</div>
       </div>
     )
   }
