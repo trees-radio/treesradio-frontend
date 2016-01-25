2b49e0506387ff5f844e204b515155861b6cb240
Zach Buttram <zach.buttram@gmail.com>
2016-01-25 01:04:17 -0800

    font-awesome fix

--- webpack.config.js
+++ webpack.config.js
@@ -32,15 +32,13 @@ module.exports = {
         // loader: ExtractTextPlugin.extract("style", "css")
       },
       {
-        test: /\.png$/,
-        loader: 'file'
+        test: /\.less$/,
+        loader: 'style!css!less'
       },
       {
-        test: /\.jpg$/,
-        loader: 'file'
-      },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        loader: 'url?limit=100000&name=[name].[ext]'
+      }
     ]
   },
   plugins: [
