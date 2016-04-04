3fd765080dd9a9fc530622f5588e4e8a0a5c75e9
GryphonEDM <griffinvmichael@gmail.com>
2016-04-04 01:00:20 -0700

    Test
    * added link to subreddit in about page
    
    * google analytics
    
    * mobile
    
    * 123

--- app/Main.js
+++ app/Main.js
@@ -1170,7 +1170,7 @@ var Main = React.createClass({
               <div className="container-fluid">
                   <div className="row">
             {/* Video Component */}
-                      <div className="col-lg-9 col-md-9 col-sm-9 col-xs-9 no-float" id="videotoplevel">
+                      <div className="col-lg-9 col-md-9 col-sm-9 col-xs-12 no-float" id="videotoplevel">
                           <div id="vidcontainer" className="">
                             <Video
                               controls={this.state.controls}
