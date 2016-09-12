213ecaadefcb24ee88dfe16e708d7099409b7ef7
Zach Buttram <zach.buttram@gmail.com>
2016-09-11 23:43:20 -0700

    some new component handling

--- app/index.js
+++ app/index.js
@@ -2,7 +2,11 @@ import React from 'react'; //eslint-disable-line
 //React is required here for ReactDOM to work properly, has already been tested
 import ReactDOM from 'react-dom';
 
-import Main from './Main';
+import Main from './components/Main';
+
+// (S)CSS
+import './Main.scss';
+
 require("font-awesome-webpack");
 require('babel-polyfill');
 
