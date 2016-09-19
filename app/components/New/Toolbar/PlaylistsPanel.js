0389ac8b25f1652e5afeac487e0841c2cbdd22d2
Zach Buttram <zach.buttram@gmail.com>
2016-09-18 22:51:39 -0700

    displaying playlist videos

--- app/components/New/Toolbar/PlaylistsPanel.js
+++ app/components/New/Toolbar/PlaylistsPanel.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {observable} from 'mobx';
+import {observable, toJS} from 'mobx';
 import {observer} from 'mobx-react';
 // import classNames from 'classnames';
 // import fbase from 'stores/fbase';
 import playlists from 'stores/playlists';
 import Modal from 'components/utility/Modal';
 import toast from 'utils/toast';
+import moment from 'moment';
 
 export default @observer class PlaylistsPanel extends React.Component {
 
@@ -33,10 +34,74 @@ export default @observer class PlaylistsPanel extends React.Component {
     playlists.selectPlaylist(i);
   }
 
+  search() {
+    var query = this._search.value;
+    this._search.value = '';
+    playlists.runSearch(query);
+  }
+
   render() {
     var mainClass = this.props.open ? "playlists-panel-open" : "playlists-panel-closed";
 
-    var playlistsCurrentView;
+    var content;
+
+    if (playlists.searching) {
+      content = <i className="fa fa-spin fa-4x fa-circle-o-notch"></i>;
+    } else if (playlists.openSearch) {
+      let list = playlists.search.map((video, i) => {
+        var playlistPosClass = Number.isInteger(i / 2) ? "playlist-item-1" : "playlist-item-2";
+        var url = `https://www.youtube.com/watch?v=${video.id}`;
+        var duration = moment.duration(video.contentDetails.duration);
+        var hours = duration.hours();
+        var hoursDisplay = '';
+        if (hours > 0) {
+          hoursDisplay = hours+'h ';
+        }
+        var mins = duration.minutes();
+        var secs = '0'+duration.seconds();
+        var humanDuration = hoursDisplay + mins +':'+ secs.substr(-2);
+        return (
+            <li className={playlistPosClass} key={i}>
+              <a target="_blank" href={url}><img className="pl-thumbnail" src={video.snippet.thumbnails.default.url} /></a>
+              <span className="pl-media-title">{video.snippet.title}</span>
+              <span className="pl-time"><i className="fa fa-clock-o"></i> {humanDuration}</span>
+              <span className="pl-channel">{video.snippet.channelTitle}</span>
+              <i onClick={() => playlists.addFromSearch(i)} className="fa fa-2x fa-plus add-to-playlist-btn"></i>
+            </li>
+        )
+        // console.log('video', toJS(video));
+      });
+      content = (<ul id="playlist-ul">{list}</ul>);
+    } else if (playlists.playlist.length > 0) {
+      let list = playlists.playlist.map((video, i) => {
+        var playlistPosClass = Number.isInteger(i / 2) ? "playlist-item-1" : "playlist-item-2";
+        var humanDuration = "Unknown";
+        if (video.duration) {
+          // console.log(item.duration);
+          var duration = moment.duration(video.duration);
+          var hours = duration.hours();
+          var hoursDisplay = "";
+          if (hours > 0) {
+            hoursDisplay = hours + "h ";
+          }
+          var mins = duration.minutes();
+          var secs = "0" + duration.seconds();
+          humanDuration = hoursDisplay + mins +':'+ secs.substr(-2);
+        }
+
+        return (
+          <li className={playlistPosClass} key={i}>
+            <a target="_blank" href={video.url}><img className="pl-thumbnail" src={video.thumb} /></a>
+            <span className="pl-media-title">{video.title}</span>
+            <span className="pl-time"><i className="fa fa-clock-o"></i> {humanDuration}</span>
+            <span className="pl-channel">{video.channel}</span>
+            <i onClick={() => {}} className="fa fa-2x fa-trash remove-from-playlist-btn"></i>
+            <i onClick={() => {}} className="fa fa-2x fa-arrow-up pl-move-to-top"></i>
+          </li>
+        );
+      });
+      content = (<ul id="playlist-ul">{list}</ul>);
+    }
 
     var playlistsList = playlists.playlistNames.map((name, i) => (
       <li key={i}>
@@ -48,7 +113,7 @@ export default @observer class PlaylistsPanel extends React.Component {
     return (
       <div id="playlists-panel" ref="playlists-panel" className={mainClass}>
         <div id="playlists-panel-head">
-          <input type="text" id="playlist-search-box" ref="searchbox" placeholder="Search YouTube" className="form-control" onKeyPress={() => {}} />
+          <input type="text" id="playlist-search-box" ref={c => this._search = c} placeholder="Search YouTube" className="form-control" onKeyPress={e => this.onEnterKey(e, () => this.search())} />
           <div className="btn-group" id="playlist-btn">
             <a className="btn btn-primary dropdown-toggle" id="playlist-dropdown" data-toggle="dropdown" href="#"><p id="pl-current-playlist">{playlists.selectedPlaylistName}</p></a>
             <ul className="dropdown-menu" id="pl-dd-menu">
@@ -60,7 +125,7 @@ export default @observer class PlaylistsPanel extends React.Component {
           <span onClick={() => {}} className="playlist-shuffle-btn fa fa-random fa-3x"></span>
         </div>
         <div id="playlists-panel-display">
-          {playlistsCurrentView}
+          {content}
         </div>
         <Modal isOpen={this.addingPlaylist} hideModal={() => this.addingPlaylist = false} title="Adding Playlist" leftButton={() => this.addPlaylist()} leftButtonText="Add Playlist">
           <p>Choose a name for your new playlist:</p>
--- app/stores/playlists.js
+++ app/stores/playlists.js
@@ -3,6 +3,10 @@ import toast from 'utils/toast';
 import fbase from 'libs/fbase';
 import profile from 'stores/profile';
 import localforage from 'localforage';
+import axios from 'axios';
+import moment from 'moment';
+
+const ytApiKey = 'AIzaSyDXl5mzL-3BUR8Kv5ssHxQYudFW1YaQckA';
 
 export default new class Playlists {
   constructor() {
@@ -10,28 +14,34 @@ export default new class Playlists {
       if (user !== null) {
         this.uid = user.uid;
         this.ref = fbase.database().ref('playlists').child(this.uid);
-          this.stopSync = this.ref.orderByKey().on('value', snap => {
-            var playlists = [];
-            var index = 0;
-            var toSelect;
+        this.stopSync = this.ref.orderByKey().on('value', snap => {
+          var playlists = [];
+          snap.forEach(playlist => {
+            var data = playlist.val();
+            data.key = playlist.key;
+            playlists.push(data);
+          });
+          this.playlists = playlists;
+          console.log('playlist data', playlists);
+
+          if (!this.init) {
             localforage.getItem('selectedPlaylist').then(selectedPlaylist => {
-              snap.forEach(playlist => {
-                var data = playlist.val();
-                data.key = playlist.key;
-                if (selectedPlaylist && data.key === selectedPlaylist) {
-                  toSelect = index;
+              var toSelect;
+              playlists.forEach((playlist, i) => {
+                if (selectedPlaylist && playlist.key === selectedPlaylist) {
+                  toSelect = i;
                 }
-                playlists.push(data);
-                index++;
               });
-              console.log('playlist data', playlists);
-              this.playlists = playlists;
               if (toSelect) {
                 this.selectPlaylist(toSelect);
+              } else {
+                this.selectPlaylist(0);
               }
-              this.init = true;
             });
-          });
+          }
+
+          this.init = true;
+        });
       } else {
         this.init = false;
         if (this.stopSync) {
@@ -48,7 +58,10 @@ export default new class Playlists {
   @observable init = false;
   @observable playlists = [];
   @observable selectedPlaylist = 0;
+  @observable selectedPlaylistKey = '';
   @observable playlist = [];
+  @observable searching = false;
+  @observable search = [];
 
   addPlaylist(name) {
     this.ref.push({name, entries: []});
@@ -60,31 +73,104 @@ export default new class Playlists {
   }
 
   selectPlaylist(index) {
+    this.search = []; //clear any searches
     if (this.stopPlaylistSync) {
       this.stopPlaylistSync();
     }
     this.selectedPlaylist = index;
-    var key = this.playlists[index].key;
-    fbase.database().ref('private').child(this.uid).child('selectedPlaylist').set(key);
-    localforage.setItem('selectedPlaylist', key);
-    this.stopPlaylistSync = this.ref.child(key).child('entries').orderByKey().on('value', (snap) => {
-      var playlist = [];
-      // console.log(snap.forEach);
-      if (snap) {
-        snap.forEach(entry => {
-          playlist.push(entry.val());
-        });
-      }
-
-      this.playlist = playlist;
-    });
+    if (this.playlists[index]) {
+      var key = this.playlists[index].key;
+      this.selectedPlaylistKey = key;
+      fbase.database().ref('private').child(this.uid).child('selectedPlaylist').set(key);
+      localforage.setItem('selectedPlaylist', key);
+      this.stopPlaylistSync = this.ref.child(key).child('entries').orderByKey().on('value', (snap) => {
+        var playlist = [];
+        // console.log(snap.forEach);
+        if (snap) {
+          snap.forEach(entry => {
+            playlist.push(entry.val());
+          });
+        }
+        this.playlist = playlist;
+      });
+    }
   }
 
   @computed get selectedPlaylistName() {
     if (this.playlists[this.selectedPlaylist]) {
       return this.playlists[this.selectedPlaylist].name;
     } else {
-      return "Select a Playlist";
+      return "Create or Select a Playlist";
     }
+  }
+
+  @computed get hasPlaylist() {
+    if (this.playlists[this.selectedPlaylist]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @computed get openSearch() {
+    if (this.search.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  runSearch(query) {
+    this.search = [];
+    this.searching = true;
+    axios.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        part: 'snippet',
+        maxResults: '25',
+        type: 'video',
+        videoEmbeddable: 'true',
+        key: ytApiKey,
+        q: query
+      }
+    }).then(resp => {
+      var search = resp.data.items.map(data => data.id.videoId);
+      var ids = search.reduce((p, c) => p + ',' + c, '');
+      axios.get('https://www.googleapis.com/youtube/v3/videos', {
+        params: {
+          id: ids,
+          part: 'contentDetails,snippet',
+          key: ytApiKey
+        }
+      }).then(resp => {
+        this.search = resp.data.items;
+        this.searching = false;
+      });
+    });
+  }
+
+  addFromSearch(index) {
+    if (!this.hasPlaylist) {
+      toast.error("You don't have a selected playlist!");
+      return;
+    }
+    var video = this.search[index];
+    var url = `https://www.youtube.com/watch?v=${video.id}`;
+    var title = video.snippet.title;
+    var thumb = video.snippet.thumbnails.default.url;
+    var channel = video.snippet.channelTitle;
+    var duration = moment.duration(video.contentDetails.duration).valueOf();
+
+    var obj = {
+      url,
+      title,
+      thumb,
+      channel,
+      duration
+    }
+
+    var newPlaylist = this.playlist.slice();
+    newPlaylist.unshift(obj);
+    this.ref.child(this.selectedPlaylistKey).child('entries').set(newPlaylist);
+    toast.success(`Added ${title} to playlist ${this.selectedPlaylistName}.`);
   }
 }
