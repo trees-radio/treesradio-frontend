895833dea2851daa68762fb9ff4974df8f9ac73a
Zach Buttram <zach.buttram@gmail.com>
2016-09-20 00:20:58 -0700

    avatars grabbing

--- app/libs/avatars.js
+++ app/libs/avatars.js
@@ -1,11 +1,53 @@
+import {observable, computed} from 'mobx';
+import fbase from 'libs/fbase';
+import moment from 'moment';
 
+export default new class Avatars {
+  constructor() {
 
-export default class Avatars {
-  constructor(username) {
-    this.defaultAvatarUrl = `//tr-avatars.herokuapp.com/avatars/50/${username}.png`;
   }
 
-  get avatar() {
-    return this.defaultAvatarUrl;
+  defaultAvatar(username) {
+    return `//tr-avatars.herokuapp.com/avatars/50/${username}.png`;
   }
+
+  @observable users = {};
+  @observable attemptedUsers = {};
+
+  loadAvatar(username) {
+    var defaultAvatar = this.defaultAvatar(username);
+    if (!this.users[username]) {
+      this.users[username] = defaultAvatar;
+    }
+    var hasDefault = this.users[username] === defaultAvatar;
+    console.log('hasDefault', hasDefault);
+    if (hasDefault) {
+      var threshold = moment.unix().subtract(1, 'minutes');
+      var hasNoAttempt = !this.attemptedUsers[username];
+      var attemptBeforeThreshold = moment.unix(this.attemptedUsers[username]).isBefore(threshold);
+      console.log('hasNoAttempt', hasNoAttempt, 'attemptBeforeThreshold', attemptBeforeThreshold);
+      if (hasNoAttempt || attemptBeforeThreshold) {
+        fbase.database().ref('avatars').child(username).once('value', (snap) => {
+          var avatar = snap.val();
+          console.log('avatar', avatar);
+          if (avatar) {
+            var image = new Image();
+            image.onload = () => {
+              console.log('avatar chosen');
+              this.users[username] = avatar;
+            }
+            image.onerror = (e) => {
+              console.log('avatar error', e);
+              this.error = true;
+            }
+            image.src = avatar;
+          }
+        });
+        this.attemptedUsers[username] = moment.unix();
+      }
+    }
+  }
+
+
+
 }
