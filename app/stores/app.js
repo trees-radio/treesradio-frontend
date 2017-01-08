2f41e4f067f053ec2dcdac291b5c6a239d2a0ca1
Zach Buttram <zach.buttram@gmail.com>
2017-01-07 22:44:14 -0800

    remove unused socket code

--- app/stores/app.js
+++ app/stores/app.js
@@ -4,15 +4,8 @@ import socket from 'utils/socket';
 
 export default new class App {
   constructor() {
-    socket.on('connect', () => {
-      this.socket = true;
-    });
-    socket.on('disconnect', () => {
-      this.socket = false;
-    });
+    
   }
-
-  @observable socket = false;
 
   @computed get init() {
     if (profile.connected) {
