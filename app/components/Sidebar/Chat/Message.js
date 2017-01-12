2fcc9df8f8d4abda716333a631f8caa6369bfbb2
Tyler Hardison <tyler@seraph-net.net>
2017-01-12 14:45:23 -0800

    Ensure that inline images are from "trusted" domains.

--- app/components/Sidebar/Chat/Message.js
+++ app/components/Sidebar/Chat/Message.js
@@ -6,8 +6,20 @@ const expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-
 const regex = new RegExp(expression);
 
 // regex for detecting an inline image.
-const imgexpr = /(https?:)?\/\/?[^'"<>]+?\.(jpg|jpeg|gif|png)/gi;
+const imgexpr = /(https?:)?\/\/?[^'"<>]+?\.(jpg|jpeg|gif|png)/i;
 const imgregex = new RegExp(imgexpr);
+
+// XXX Maybe we should move this to a library?
+// regex for accepted image domains
+const domexpr = /^(https?:)?\/\/?(.*?)\//i;
+const domregex = new RegExp(domexpr);
+
+// This is temporary, this should be stored in firebase.
+const alloweddoms = {
+    'i.imgur.com': true,
+    'imgur.com': true,
+    'i.reddituploads.com': true
+};
 
 const emojifyOptions = {
   styles: {
@@ -22,17 +34,20 @@ const Message = props => {
 
   let result = tokens.map((tkn, i) => {
     // Is this an image link?
-    if ( tkn.match(imgregex) ) {
+    if ( tkn.match(imgregex) && alloweddoms[tkn.match(domregex)[2]] ) {
       return <span key={i}> <img src={tkn} className='inline-image'/> </span>;
-    } else if (tkn.match(regex)) {
+    } 
+    // Is this a plain URL?
+    else if (tkn.match(regex)) {
       let link = tkn;
       if (!link.slice(0, 4) === 'http') {
         link = `http://${link}`;
       }
       return <span key={i}> <a href={link} target='_blank'>{tkn}</a> </span>;
-    } else {
-      return <span key={i}> {emojify(tkn, emojifyOptions)} </span>;
     }
+    
+    return <span key={i}> {emojify(tkn, emojifyOptions)} </span>;
+
   });
 
   return <div>{result}</div>;
