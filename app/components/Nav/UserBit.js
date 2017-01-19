24468620fae4aebcc7fff411fd299bbaeef6c62b
Zach Buttram <zach.buttram@gmail.com>
2017-01-18 19:16:25 -0800

    open region check in another tab

--- app/components/Nav/UserBit.js
+++ app/components/Nav/UserBit.js
@@ -77,7 +77,7 @@ export default class UserBit extends React.Component {
               </li>
               <li onClick={() => {}}><a href="#"><i className="fa fa-envelope"></i> Change Email</a></li>
               <li onClick={() => {}}><a href="#"><i className="fa fa-key"></i> Change Password</a></li>
-              <li ><a href={`https://polsy.org.uk/stuff/ytrestrict.cgi?ytid=${playing.data.info.url}`}><i className="fa fa-youtube"></i> Region Check</a></li>
+              <li ><a href={`https://polsy.org.uk/stuff/ytrestrict.cgi?ytid=${playing.data.info.url}`} target='blank'><i className="fa fa-youtube"></i> Region Check</a></li>
               <li onClick={() => profile.logout()}><a href="#"><i className="fa fa-sign-out"></i> Logout</a></li>
             </ul>
           </div>
