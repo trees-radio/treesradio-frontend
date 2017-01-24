8f749249c6cb70999314002e35f4a858d07a25ff
Zach Buttram <zach.buttram@gmail.com>
2017-01-23 23:59:56 -0800

    cleanup event handling

--- app/libs/events.js
+++ app/libs/events.js
@@ -8,32 +8,3 @@ export function send(type, data={}) {
     return null;
   }
 }
-
-
-// was too high when I wrote this, the type isn't even used in the constructor
-const listeners = {};
-
-class EventListener {
-  handlers = [];
-
-  constructor(type, ref) {
-    fbase.database().ref(ref).on('child_added', snap => this.handlers.forEach(handler => handler(snap)));
-  }
-
-  addHandler(handler) {
-    const index = this.handlers.push(handler) - 1;
-    return () => {
-      this.handlers[index] = null;
-    }
-  }
-}
-
-export function register(type, ref, handler) {
-  if (!listeners[type]) {
-    listeners[type] = new EventListener(type, ref);
-  }
-
-  const listener = listeners[type];
-  
-  return listener.addHandler(handler);
-}
