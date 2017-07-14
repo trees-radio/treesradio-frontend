d51a3578d2f7cc73f3049a4bf45763bf4bdc407a
Tyler Hardison <yellerjeep@gmail.com>
2017-07-14 07:54:56 -0700

    Cleanup debugging messages.

--- app/libs/mention.js
+++ app/libs/mention.js
@@ -4,10 +4,7 @@ import audioNotify from "libs/audioNotify";
 let BOUNCE = Date.now()/1000;
 
 export default function mention(everyone, username) {
-  console.log(BOUNCE);
-  //if ( (Date.now() / 1000) - BOUNCE > 5 ) {
-    BOUNCE = Date.now()/1000;
-    setTimeout(() => toast.info(`You were mentioned by ${username}.`));
-    audioNotify.play();
-  //}
+  BOUNCE = Date.now()/1000;
+  setTimeout(() => toast.info(`You were mentioned by ${username}.`));
+  audioNotify.play();
 }
