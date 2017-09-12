fee5e28689f9ee51a17509b772d62a8f6b781331
Tyler Hardison <yellerjeep@gmail.com>
2017-09-12 08:56:21 -0700

    Adjust grid

--- app/components/Sidebar/karan.js
+++ app/components/Sidebar/karan.js
@@ -23,10 +23,10 @@ export default class Karan extends React.Component {
         return (
             <Modal isOpen={this.state.modalIsOpen} hideModal={() => this.toggleModal} title="In Memoriam">
                 <div className="row">
-                    <div className="col-md-2">
+                    <div className="col-md-3">
                         <img src="//i.imgur.com/NgqxUDF.jpg" width="100%"/>
                     </div>
-                    <div className="col-md-10">
+                    <div className="col-md-9">
                         <h4>In July of 2017, TreesRadio lost a moderator and a great contributor to our community.</h4>
                         <p>Saeft, or Karan as some of us came to know him, was a wonderful human being and a great friend to many of
                             us here at TR. His presence was one of love, respect, and a lightheartedness that was unmatched. His love
