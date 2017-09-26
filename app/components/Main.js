3f175032ffda6d61cbb688f32339d74cda1e5159
Tyler Hardison <yellerjeep@gmail.com>
2017-09-25 18:07:50 -0700

    c'mon

--- app/components/Main.js
+++ app/components/Main.js
@@ -37,8 +37,9 @@ export default (class Main extends React.Component {
         <div className="main-load">
           <div className="container main-loadingcard">
             <div className="main-vcenter">
-                <div className="main-center">{randmsg[Math.floor(Math.random() * randmsg.length)]}</div>
-                <div className="main-center"><i className="fa fa-spin fa-2x fa-circle-o-notch" /></div>
+                <div className="main-center">{randmsg[Math.floor(Math.random() * randmsg.length)]}
+                  <br/>
+                  <i className="fa fa-spin fa-2x fa-circle-o-notch" /></div>
             </div>
           </div>
         </div>
