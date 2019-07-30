159faea805d3cbc24ae7b1f65cf49fce674019dc
tyler@localhost <tyler@localhost>
2019-07-30 11:40:10 -0700

    fix spirittoke and undefined error

---
+++ app/components/Nav/HypeProgress.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {observer} from "mobx-react";
+
+import hypetimer from "stores/hype";
+
+const hypetime = 6 * 60 * 60;
+@observer
+export default class HypeProgress extends React.Component {
+  getHyped = () => {
+    if (hypetimer.hypePercentageCharged == 100) hypetimer.getHyped();
+  };
+  render() {
+    return (
+      <div className="hypepb" style={{visibility: hypetimer.lasthype > 0 ? true : false}}>
+        <div className="hypeprogress" style={{width: hypetimer.hypePercentageCharged + "%"}}>
+          <a
+            className="hypedone"
+            onClick={this.getHyped}
+            text={
+              "Hype Recharging: " + convertHoursMinutesSeconds(hypetime - hypetimer.secondsfromhype)
+            }
+          >
+            {hypetimer.hypePercentageCharged == 100
+              ? "Hype Ready!"
+              : convertHoursMinutesSeconds(hypetime - hypetimer.secondsfromhype)}
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+function convertHoursMinutesSeconds(time) {
+  var d = Number(time);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor((d % 3600) / 60);
+  var s = Math.floor((d % 3600) % 60);
+  return `${h}:${leadingZero(m, 2)}:${leadingZero(s, 2)}`;
+}
+
+function leadingZero(num, size) {
+  var s = "0000" + num;
+  return s.substr(s.length - size);
+}
---
+++ app/stores/hype.js
@@ -0,0 +1,64 @@
+import {computed, observable, autorun} from "mobx";
+import fbase from "libs/fbase";
+import {send} from "libs/events";
+import profile from "./profile";
+import epoch from "utils/epoch";
+
+const hypetime = 6 * 60 * 60; // 6 hours
+
+export default new (class HypeTimer {
+  constructor() {
+    autorun(() => {
+      const hypetimer = this;
+      var recheck = setInterval(function() {
+        if (profile.uid == false) return;
+        clearInterval(recheck);
+        fbase
+          .database()
+          .ref("/hypes")
+          .child(profile.uid)
+          .once("value")
+          .then(snap => {
+            var hypesnap = snap.val();
+            if (hypesnap != null) {
+              hypetimer.lasthype = hypesnap.lasthype;
+              hypetimer.checkTimer();
+            } else {
+              hypetimer.lasthype = epoch() - hypetime;
+              hypetimer.checkTimer();
+            }
+          });
+        fbase
+          .database()
+          .ref("/hypes")
+          .child(profile.uid)
+          .on("value", snap => {
+            var hypesnap = snap.val();
+            if (hypesnap != null) {
+              hypetimer.lasthype = hypesnap.lasthype;
+              hypetimer.checkTimer();
+            } else {
+              hypetimer.lasthype = epoch() - hypetime;
+              hypetimer.checkTimer();
+            }
+          });
+      }, 10000);
+      setInterval(function() {
+        hypetimer.checkTimer();
+      }, 1000);
+    });
+  }
+
+  @observable lasthype = epoch();
+  @observable hypePercentageCharged = 0;
+  @observable secondsfromhype = 0;
+  checkTimer() {
+    var timeleft = Math.round(((epoch() - this.lasthype) / hypetime) * 100);
+    this.secondsfromhype = epoch() - this.lasthype;
+    if (timeleft > 100) timeleft = 100;
+    this.hypePercentageCharged = timeleft;
+  }
+  getHyped() {
+    send("chat", {mentions: [], msg: "/hype"});
+  }
+})();
