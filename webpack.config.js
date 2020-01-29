29d7d0aeb94f5bb3a2ac454bc83e5e1bdca7bec4
TweeZee <vxenemyxv@live.de>
2020-01-29 23:10:39 +0100

    reworked navigation bar into more flexible grid

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 // this is only here because this is webpack's default file name, but we really want to use the dev config here
 var config;
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV === "development") {
   config = require("./webpack.dev.config.js");
 } else {
   config = require("./webpack.prod.config.js");
