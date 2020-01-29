f7e5c0e6251db53483746f252cadc004b1558cd4
TweeZee <vxenemyxv@live.de>
2020-01-29 23:10:30 +0100

    hype button, toke button & user bit are now more responsive

--- app/components/Nav/TokeTimer.js
+++ app/components/Nav/TokeTimer.js
@@ -5,35 +5,38 @@ import toketimer from "stores/toke";
 
 @observer
 export default class TokeTimer extends React.Component {
-  joinToke = () => {
-    toketimer.joinToke();
-  };
-  render() {
-    return (
-      <div
-        style={{
-          display: "inline-block",
-          "margin-right": "15px",
-          "margin-top": "7px"
-        }}
-      >
-        <div>
-          <a
-            className={toketimer.tokeUnderway ? "btn btn-warning" : "btn btn-success"}
-            onClick={this.joinToke}
-          >
-            Toke
-            <span style={{display: toketimer.tokeUnderway ? "inherit" : "none"}}>
+    joinToke = () => {
+        toketimer.joinToke();
+    };
+
+    render() {
+        return (
+            <div
+                style={{
+                    display: "inline-block",
+                    "margin-right": "1vw",
+                    "margin-top": ".75vh",
+                    "height": "1.5vh"
+                }}
+            >
+                <div>
+                    <a
+                        id="toke-button"
+                        className={toketimer.tokeUnderway ? "btn btn-warning" : "btn btn-success"}
+                        onClick={this.joinToke}>
+                        Toke
+                        <span style={{display: toketimer.tokeUnderway ? "inherit" : "none"}}>
               &nbsp;in: <b>{millisToMinutesAndSeconds(toketimer.remainingTime)}</b>
             </span>
-          </a>
-        </div>
-      </div>
-    );
-  }
+                    </a>
+                </div>
+            </div>
+        );
+    }
 }
+
 function millisToMinutesAndSeconds(millis) {
-  var minutes = Math.floor(millis / 60000);
-  var seconds = ((millis % 60000) / 1000).toFixed(0);
-  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 }
