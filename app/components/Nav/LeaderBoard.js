ae99e4a855b0b201882370027f92ae639110f4c6
Tyler Hardison <tyler@stninc.com>
2020-04-28 10:02:00 -0700

    CODE: leaderboard tweaks

--- app/components/Nav/LeaderBoard.js
+++ app/components/Nav/LeaderBoard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {observer} from "mobx-react";
+import { observer } from "mobx-react";
 
 import LeadersBoard from "stores/leaderboard";
 import UserAvatar from "components/utility/User/UserAvatar";
@@ -7,18 +7,36 @@ import UserName from "components/utility/User/UserName";
 
 @observer
 export default class LeaderBoard extends React.Component {
-    
+
     render() {
         let leadersarray = LeadersBoard.leaders;
         let leaders = leadersarray.map((leader, index) => {
+            let now = Math.floor(Date.now() / 1000);
+            let time = leader.timestamp;
+            let ago = Math.floor((now - time) / 60) + " minutes ago";
+            if (now - time > 3600) {
+                let hours = Math.floor((now - time) / 3600);
+                let s = "s";
+                if (hours == 1) s = "";
+                ago = `${hours} hour${s} ago`;
+            }
+            let multiplier = 1;
+            let basescore = leader.likes + leader.grabs - Math.floor(leader.dislikes / 2);
+
+            if (leader.onlineents) {
+                multiplier = basescore / leader.onlineents;
+            }
+
+            let actualscore = Math.ceil(basescore * multiplier);
+
             return (
                 <div key={index} className="leaderboard-row">
-                    <span className="leaderboard-cell-rank"><i className="fa fa-trophy"></i> {index+1}:</span>
+                    <span className="leaderboard-cell-rank"><i className="fa fa-trophy"></i> {index + 1}:</span>
                     <span className="leaderboard-cell-avatar"><UserAvatar uid={leader.uid} /></span>
-                    <span className="leaderboard-cell-username"><UserName uid={leader.uid} /></span>
+                    <span className="leaderboard-cell-username"><UserName uid={leader.uid} /><br />
+                        <strong>{actualscore}</strong>  Likes 👍 {ago}</span>
                     <span className="leaderboard-cell-songtitle"><a target="_blank" rel="noopener noreferrer" href={leader.url}>{leader.title}</a></span>
-                    <span className="leaderboard-cell-songthumb"><img src={leader.thumb}/></span>
-                    <br/>
+                    <span className="leaderboard-cell-songthumb"><img src={leader.thumb} /></span>
                 </div>
             )
         });
