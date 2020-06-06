9d0db702989db5339ac36705f3d56eadaca99e38
Leon Brombeer <vxenemyxv@live.de>
2020-06-06 16:02:23 +0000

    FEATURE: hacked together desktop notifications. This is barely tested and only works with mentions atm

--- app/libs/mention.js
+++ app/libs/mention.js
@@ -1,8 +1,30 @@
 import toast from "utils/toast";
 import audioNotify from "libs/audioNotify";
 import profile from "../stores/profile";
+import favicon from "../img/favicon.png"
 
 export default function mention(everyone, username) {
-  setTimeout(() => toast.info(`You were mentioned by ${username}.`));
-  if (profile.notifications) audioNotify.play();
+
+    if (profile.username !== username) {
+
+
+        setTimeout(() => {
+            if (profile.desktopNotifications) {
+                let options = {
+                    icon: favicon,
+                    badge: favicon,
+                    body: 'You were mentioned by ' + username + '.',
+                    renotify: true,
+                    silent: true,
+                }
+
+                let n = new Notification("TreesRadio", options);
+                n.onclick = () => document.getElementById("chatinput").focus();
+
+            } else {
+                toast.info(`You were mentioned by ${username}.`);
+            }
+        });
+        if (profile.notifications) audioNotify.play();
+    }
 }
