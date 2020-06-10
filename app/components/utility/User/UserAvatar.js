0d2c57f09f6056286190150092f787068af58623
Leon Brombeer <vxenemyxv@live.de>
2020-06-10 17:18:46 +0000

    REFACTOR: refactored a lot, most notably mixins and media queries now have their own scss files + scss now has a new package structure FIX: fixed login return value & removed old conversion code OTHER: chat can no longer be scrolled by non-logged-in users

--- app/components/utility/User/UserAvatar.js
+++ app/components/utility/User/UserAvatar.js
@@ -5,58 +5,61 @@ import VisibilitySensor from "react-visibility-sensor";
 import EMPTY_IMG from "../../../img/nothing.png";
 
 export default class UserAvatar extends React.Component {
-  _isMounted = false;
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: true
-    };
-    this.getAvatar();
-  }
+    _isMounted = false;
 
-  componentDidMount() {
-    this._isMounted = true;
-  }
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-  getAvatar = async () => {
-    const fallback = await defaultAvatar(this.props.uid);
-
-    this.setState(
-      {
-        avatar: fallback
-      },
-      () => {
-        if ( this._isMounted ) 
-        listenAvatar(this.props.uid, snap => {
-          this.setState({
-            avatar: snap.val() || fallback
-          });
-        });
-      }
-    );
-  };
-
-  onVisibility = isVisible => this.setState({visible: isVisible});
-
-  render() {
-    let avatar;
-    if (imageWhitelist(this.state.avatar)) {
-      avatar = this.state.avatar;
-    } else {
-      avatar = EMPTY_IMG;
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: true
+        };
+        this.getAvatar();
     }
 
-    avatar = avatar.replace("http:", "https:");
+    componentDidMount() {
+        this._isMounted = true;
+    }
 
-    let style = {};
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
 
-    return (
-        <span className={this.props.className}>
+    getAvatar = async () => {
+        const fallback = await defaultAvatar(this.props.uid);
+
+        this.setState(
+            {
+                avatar: fallback
+            },
+            () => {
+                if (this._isMounted)
+                    listenAvatar(this.props.uid, snap => {
+                        this.setState({
+                            avatar: snap.val() || fallback
+                        });
+                    });
+            }
+        );
+    };
+
+    onVisibility = isVisible => this.setState({visible: isVisible});
+
+    render() {
+        let avatar;
+        if (imageWhitelist(this.state.avatar)) {
+            avatar = this.state.avatar;
+        } else {
+            avatar = EMPTY_IMG;
+        }
+
+        avatar = avatar.replace("http:", "https:");
+
+        let style = {};
+
+        return (
+            <span className={this.props.className}>
         <img src={avatar} className={this.props.imgClass || "avatarimg"} style={style} id="user-avatar"/>
         <VisibilitySensor onChange={this.onVisibility}/>
       </span>
-    );
-  }
+        );
+    }
 }
