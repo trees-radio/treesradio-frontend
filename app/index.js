de732a45288ccbcd7e7bbbc375b1a3994993fc05
Pierre Karampournis <pkarampournis@gmail.com>
2021-05-19 16:30:30 -0400

    PWA baby, the app can be installed on any mobile device. Android will prompt an install popup, whereas IOS is quieter (need to manually go to the options menu and add to homepage).
    Barebone manifest + Service worker are setup
    also, a little fix on the username dropdown width, to accomodate super small screens :)

--- app/index.js
+++ app/index.js
@@ -12,4 +12,14 @@ import 'babel-polyfill'; // required for IE
 import 'script-loader!localforage'; //needs to execute as script, ugly console warnings otherwise
 import Main from './components/Main';
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
+
 ReactDOM.render(<Main />, document.getElementById('app'));
