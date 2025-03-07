0cd6090ca96ce6d3ee654c597ffbcad6793e0111
Tyler Hardison <tylerh@evercharge.com>
2025-03-07 08:31:07 -0800

    come on you guys, let's go play football

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -10,23 +10,23 @@ module.exports = {
     "plugin:import/warnings",
     "plugin:import/typescript",
     "google",
-    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json"],
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    tsconfigRootDir: __dirname,
     sourceType: "module",
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
+    ".eslintrc.js", // Ignore the eslint config itself
   ],
   plugins: [
     "@typescript-eslint",
     "import",
   ],
   rules: {
-    "quotes": ["error", "double"],
+    "quotes": ["error", "single"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
   },
